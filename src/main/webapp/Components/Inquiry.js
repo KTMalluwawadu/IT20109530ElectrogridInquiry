48d5b2524648ea6dd945d6e40c6e5cb535f4f8ac
KTMalluwawadu <it20109530@my.sliit.lk>
2022-05-14 23:40:07 +0530

    Implement InquiryAPI

---
+++ src/main/webapp/Components/Inquiry.js
@@ -0,0 +1,142 @@
+$(document).ready(function() {
+	$("#alertSuccess").hide();
+	$("#alertError").hide();
+});
+
+$(document).on("click", "#btnSave", function(event) {
+	// Clear status msges---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+
+	// Form validation-------------------
+	var status = validateComplaintForm();
+	// If not valid
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	} 
+	
+	//if valid
+	var type = ($("#hidcomIDSave").val() == "") ? "POST" : "PUT";
+	$.ajax(
+	{
+		url : "InquiryAPI",
+		type : type,
+		data : $("#Complaint").serialize(),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onComplainSaveComplete(response.responseText, status);
+		}
+	});
+	
+	//$("#Complaint").submit();
+	
+
+});
+
+function onComplainSaveComplete(response, status) 
+{ 
+ 	if(status == "success")
+ 	{
+		var resultSet = JSON.parse(response);
+		
+		if(resultSet.status.trim() == "success")
+		{
+			$("#alertSuccess").text("Successfully saved."); 
+			$("#alertSuccess").show(); 
+			
+			$("#comGrid").html(resultSet.data); 
+		}else if(resultSet.status.trim() == "error")
+		{
+			$("#alertError").text(resultSet.data); 
+ 			$("#alertError").show();
+		}
+	}else if(status == "error")
+	{
+		 $("#alertError").text("Error while saving status."); 
+		 $("#alertError").show();
+	}else
+	{
+		 $("#alertError").text("Error while saving ."); 
+		 $("#alertError").show();
+	}
+		$("#hidcomIDSave").val(""); 
+		$("#Complaint")[0].reset(); 
+}
+
+//Update-------
+$(document).on("click", ".btnUpdate", function(event)
+	{
+		$("#hidcomIDSave").val($(this).data("itemid"));
+		$("#combox").val($(this).closest("tr").find('td:eq(1)').text()); 
+		$("#comDate").val($(this).closest("tr").find('td:eq(2)').text());
+		$("#Userid").val($(this).closest("tr").find('td:eq(3)').text()); 
+	});
+
+//delete	
+$(document).on("click", ".btnRemove", function(event) {
+
+	
+	$.ajax(
+	{
+		url : "InquiryAPI",
+		type : "DELETE",
+		data : "comId=" + $(this).data("itemid"),
+		dataType : "text",
+		complete : function(response, status)
+		{
+			onComplainDeleteComplete(response.responseText, status);
+		}
+	});
+	
+});	
+
+function onComplainDeleteComplete(response, status)
+{ 
+if (status == "success") 
+ { 
+ var resultSet = JSON.parse(response); 
+ if (resultSet.status.trim() == "success") 
+ { 
+ $("#alertSuccess").text("Successfully deleted."); 
+ $("#alertSuccess").show(); 
+ $("#comGrid").html(resultSet.data); 
+ } else if (resultSet.status.trim() == "error") 
+ { 
+ $("#alertError").text(resultSet.data); 
+ $("#alertError").show(); 
+ } 
+ } else if (status == "error") 
+ { 
+ $("#alertError").text("Error while deleting."); 
+ $("#alertError").show(); 
+ } else
+ { 
+ $("#alertError").text("Unknown error while deleting.."); 
+ $("#alertError").show(); 
+ } 
+}
+
+
+
+
+function validateComplaintForm() {
+	//Validations 
+	//USER ID
+	if ($("#Userid").val().trim() == "") {
+		return "Insert User ID!";
+	}
+
+	//COMPLAINT
+	if ($("#combox").val().trim() == "") {
+		return "PLease Insert Complaint!";
+	}
+
+	return true;
+
+}
+
---
+++ src/main/webapp/Inquiry.jsp
@@ -0,0 +1,81 @@
+<%@page import="com.Inquiry"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="java.sql.*" %>
+<%
+Class.forName("com.mysql.cj.jdbc.Driver");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+
+	<title>ElectroGrid-Complaints</title>
+	<link rel="stylesheet" href="View/bootstrap.min.css">
+	<script src="Components/jquery.min.js"></script>
+	<script src="Components/Inquiry.js"></script>
+	
+
+</head>
+<body>
+<div class="bg-light">
+     <div class="container"><div class="row"><div class="col-12">
+	
+	 <center><h1 style="font-size:380%;">ACCOUNT HOLDER INQUIRY</h1></center>
+	 
+<hr/>
+
+	 <div class="card bg-info">
+          <div class="card-body">
+	<form id = "Complaint"  method='post' action='Inquiry.jsp'>
+		
+					    
+			 <div class="mb-3">
+			  <label for="exampleFormControlInput1" class="form-label">User ID</label>
+			  <input type="text" class="form-control form-control-lg" id="Userid" name="Userid" placeholder="">
+			</div>
+			<div class="mb-3">
+			  <label for="exampleFormControlTextarea1" class="form-label">Complaints</label>
+			  <textarea class="form-control" id="combox" name="combox" rows="3"></textarea>
+			</div>
+			<div class="mb-3">
+			  <label for="exampleFormControlTextarea1" class="form-label">Date</label>
+			  <input type="date" class="form-control" id="comDate" name="comDate" ></input>
+			</div>
+			
+			
+			
+			
+		 	<input id="btnSave" name="btnSave" type="button" value="Send" class="btn btn-primary">
+		 	
+		 	
+		 	<input type="hidden" id="hidcomIDSave" name="hidcomIDSave" value="">
+		
+
+	</form>
+	</div>
+	</div>
+	<hr/>
+	
+	<div id="alertSuccess" class="alert alert-success"></div>
+			<div id="alertError" class="alert alert-danger"></div>
+	
+	
+	<hr/>
+		<br>
+	<center>
+			<div class="table table-striped " id="divItemsGrid">
+		<%
+		
+
+				Inquiry inqobj = new Inquiry(); 
+				out.print(inqobj.retriveInquiry());
+		%>
+		</div>
+			</center>
+	</div>
+	</div>
+	</div>
+	</div>
+</body>
+</html>
